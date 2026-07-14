ab71a41b646ce46eb7d5334cb93bc6fef2f7adaa
Archana-M-11 <archanamnair096@gmail.com>
2026-07-14 12:19:05 +0530

    new commit

--- string_assignment/count_character.py
+++ string_assignment/count_character.py
@@ -1,9 +1,9 @@
 '''2. Count how many times "s" appears in "Mississippi"'''
 word='Mississippi'
 count=0
-count=word.lower().count('s')
+# count=word.lower().count('s')
 for ch in word:
-    if ch=='s' or 'S':
+    if ch=='s':
         count+=1
 print('s occured for',count,'times')
     
--- string_assignment/snake_to_camel_case.py
+++ string_assignment/snake_to_camel_case.py
@@ -2,5 +2,7 @@
 10.snake_to_camel_case.py
 '''
 s=input('enter the snale case: ').split('_')
-
-
+camel=s[0]
+for ch in s[1:]:
+    camel+=ch.title()
+print('In cammelCase:',camel)    

ab22a30e189ee2508b942e6623c7036bbb519785
Archana-M-11 <archanamnair096@gmail.com>
2026-07-16 14:03:17 +0530

    commit

---
+++ Class/transaction.py
@@ -0,0 +1,27 @@
+'''
+Create a class named TransactionIterator.
+The class should:
+Accept a list of transactions through its constructor.
+Maintain the current position using an index variable.
+Implement the following special methods:
+__iter__() to return the iterator object.
+__next__() to return one transaction at a time.
+'''
+class  TransactionIterator():
+    def __init__(self,trasactions):
+        self.transactions=transactions
+        self.index=0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.index<len(self.transactions):
+             transaction = self.transactions[self.index]
+             self.index+=1
+             return transaction
+        else:
+            raise StopIteration
+transactions = ["Deposit ₹1000", "Withdraw ₹500", "Deposit ₹2000"]
+t=TransactionIterator(transactions)
+
+for transactions in t:
+    print(transactions)
